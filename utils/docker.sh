2376db9a389c4bec4f65e224a07b81f486f6a2b4
Flurin Rindisbacher <info@flurischt.ch>
2017-07-07 12:27:04 +0200

    docker.sh changes

--- utils/docker.sh
+++ utils/docker.sh
@@ -9,7 +9,7 @@
 #
 # USAGE:
 #   MAKE SURE docker.sh is in the root of the Sample repository
-#   >./docker.sh
+#   > sudo ./docker.sh
 #   container> cd workspace/sample && sbt "project sample-silver" test
 
 # get the path to the parent directory of this script
@@ -17,10 +17,10 @@ PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 # run/create a container named sample. This call will fail on the 2nd invocation
 # we map the parent directory of the current script to /home/sample/workspace in the container
-sudo docker run -td --name sample -v $PARENT_DIR:/home/sample/workspace flurischt/sample /bin/bash 2> /dev/null
+docker run -td --name sample -v $PARENT_DIR:/home/sample/workspace flurischt/sample /bin/bash 2> /dev/null
 
 # in case this is not the first invocation, the previous run failed (name already exists). Easy, just start the container
-sudo docker start sample 2> /dev/null
+docker start sample 2> /dev/null
 
 # then open an interactive shell in it
-sudo docker exec -it sample /bin/bash
+docker exec -it sample /bin/bash
